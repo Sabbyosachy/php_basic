3d0fb6ee0d47ba4663568d1367bd47dfaadc61f1
Sabbyosachy <subbyosachychakraborty@gmail.com>
2022-05-23 20:27:18 +0600

    fisrt

---
+++ hello.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+  #variable declare 
+  $varible="10";
+  $name="sam";
+  
+  #print anything echo use
+//   echo $varible;
+
+  #print a string with break
+  echo "My name is ".$name."<br>";
+  echo "I am ".$varible." years old";
+    ?>
+</body>
+</html>
